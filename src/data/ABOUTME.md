f644fe0bfd9e2e511110f2a5fa9720f9784e7f3c
mkfsn <p408865@gmail.com>
2017-05-20 16:19:28 +0800

    Angular 4!

---
+++ src/data/ABOUTME.md
@@ -0,0 +1,53 @@
+mkfsn (Pei-Ming Wu)
+===================
+
+* Programmer of C, C++, PHP, JavaScript, Python and Golang
+* Front-end and Back-end Web programmer
+
+Skill
+=====
+
+* Programming
+* Linux System management
+* Security
+
+Programming Language
+--------------------
+
+* C/C++
+* PHP
+* Python
+* Golang
+* HTML/CSS
+* JavaScript
+* Ruby
+* Perl
+
+System manage
+-------------
+
+* Linux (Gentoo, Ubuntu, CentOS)
+* Web Server (Apache, Nginx)
+* Mail Server (postfix, dovecot)
+* DNS Server (bind9)
+* FTP Server (vsftpd)
+* L3 Switch/Router
+
+Security
+--------
+
+* Web Security
+
+Projects
+========
+
+OnGoing Projects
+----------------
+
+* [Koikoi][]
+* [sousenkyo2015_members][]
+* [akb48sousenkyou]
+
+[Koikoi]: https://github.com/mkfsn/Koikoi
+[sousenkyo2015_members]: https://github.com/mkfsn/sousenkyo2015_members
+[akb48sousenkyou]: https://github.com/mkfsn/akb48sousenkyou
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var webpack = require("webpack"),
+  path = require('path');
+
+const ccpOptions = {
+  name: 'vendor',
+  filename: './vendor.bundle.js'
+};
+
+function root(__path) {
+  return path.join(__dirname, __path);
+}
+
+module.exports = {
+  entry: {
+    "vendor": "./src/vendor",
+    "app": "./src/main"
+  },
+  output: {
+    path: __dirname,
+    filename: "./[name].bundle.js"
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [
+      path.resolve('./src'),
+      'node_modules'
+    ]
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {test: /\.html$/, loader: "html-loader"},
+      {
+        test: /\.ts/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(ccpOptions),
+
+    // Takes care of warnings described at https://github.com/angular/angular/issues/11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      root('./src'), // location of your src
+      { }
+    ),
+
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true
+    })
+  ]
+}
