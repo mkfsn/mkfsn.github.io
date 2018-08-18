bef83c569ad946e066e2f7b65f0e0e1af96ee8dc
mkfsn <p408865@gmail.com>
2018-08-19 02:05:56 +0800

    rebuilding site 2018年 8月19日 日曜日 02時05分56秒 CST

--- Makefile
+++
@@ -1,6 +0,0 @@
-SRCS := ABOUTME.md
-
-.PHONY: all
-
-all:
-	pandoc $(SRCS) -f markdown -t html -s -o index.html
--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-declare var require: any;
-
-@Component({
-    selector: 'app',
-    template: require('./app.html')
-})
-export class AppComponent {}
