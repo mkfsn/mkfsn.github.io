07f8ac5fd03f52e760aecc63a56e1bd87ef72ed1
Pei-Ming Wu <p408865@gmail.com>
2020-07-20 20:53:26 +0800

    feat: add meta for Google Search Console

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Header from "../components/header"
 import Card from "../components/card"
 import Footer from "../components/footer"
@@ -10,6 +11,9 @@ export default function Home() {
             <Header />
             <Card />
             <Footer />
+            <Helmet>
+                <meta name="google-site-verification" content="glBq2kB4AN8DIhnjIQm3n0dOxPf9NYcBQB8SPlBskQ4" />
+            </Helmet>
         </main>
     );
 }
